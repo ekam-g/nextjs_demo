b1c5ac3e2369183b7211715969492d30b7388825
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-07-11 22:35:15 -0400

    Java Script sucks

--- pages/input/[id].js
+++ pages/input/[id].js
@@ -1,18 +1,38 @@
 import {useRouter} from 'next/router'
+import {useState} from "react";
 
-export default function index() {
+
+export default function
+
+
+    index() {
+
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const router = useRouter()
     const {id} = router.query
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    let [likes, setLikes] = useState()
+    likes = 0;
+
+    function handleClick() {
+        setLikes(likes + 1)
+    }
+
+
 
     return (
         <div>
+            <title>Input Page</title>
             <header>
                 <h1>This is the data page</h1>
                 <p>here is data {id}</p>
             </header>
+            <button onClick={() => router.push('/')}>Go to home</button>
+            <area/>
+            <button onClick={handleClick}>Likes ({likes})</button>
+
         </div>
-    )
+    );
 
 }
