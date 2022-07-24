68c29f817268cdc32182e06ea5348d9fab748064
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-07-24 15:36:45 -0400

    Java Script sucks

--- pages/cool.js
+++ pages/cool.js
@@ -1,7 +1,13 @@
+import Head from "next/head";
+
 export default function cool() {
 
     return (
         <div>
+            <Head>
+                <title>Cool Page In Next.JS</title>
+                <meta name= 'cool page' content='very cool content'/>
+            </Head>
             <header>
                 <h1>A heading here</h1>
                 <p>Posted by John Doe</p>
--- pages/input/[id].js
+++ pages/input/[id].js
@@ -12,13 +12,17 @@ export default function
     const router = useRouter()
     const {id} = router.query
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    let [likes, setLikes] = useState()
+    let [likes, setLikes, number] = useState()
     likes = 0;
+    number = 0;
 
     function handleClick() {
         setLikes(likes + 1)
     }
-
+    function math(){
+        number = 2000000000000 + 1234180924907594385 - 452914385723904
+        console.log(number)
+    }
 
 
     return (
@@ -28,11 +32,14 @@ export default function
                 <h1>This is the data page</h1>
                 <p>here is data {id}</p>
             </header>
-            <button onClick={() => router.push('/')}>Go to home</button>
-            <area/>
-            <button onClick={handleClick}>Likes ({likes})</button>
-
+            <div>
+                <button onClick={() => router.push('/')}>Go to home</button>
+            </div>
+            <div>
+                <button onClick={handleClick}>Likes ({math})</button>
+            </div>
         </div>
-    );
+    )
+
 
 }
